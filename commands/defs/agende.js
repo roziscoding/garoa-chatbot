15d240c21b341e5a5cec9a695f7a8cabc1cd73ff
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2018-01-11 22:13:45 -0200

    feat: add random phrases to agente function

--- commands/defs/agende.js
+++ commands/defs/agende.js
@@ -1,7 +1,17 @@
+const phrases = [
+  'Mas é agente do FBI?',
+  'Oloco, agente secreto?',
+  'Agente?! FBI, CIA ou NSA?',
+  'Agente federal?',
+  'Agente? Mas é o James Bond?'
+]
+
 const fun = () => {
-  return 'Mas é agente do FBI?'
+  const random = Math.floor(Math.random() * phrases.length)
+  return phrases[random]
 }
 
-fun.regex = /agente/
+fun.regex = /agente/ig
+fun.reply = true
 
 module.exports = fun
