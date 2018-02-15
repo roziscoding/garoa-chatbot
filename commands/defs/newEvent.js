d255b34c3169dcee2443631798dcd55c20b3c958
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2018-02-14 23:48:31 -0200

    Add database backed events

---
+++ commands/defs/newEvent.js
@@ -0,0 +1,33 @@
+'use strict'
+const moment = require('moment')
+
+const makeCreatedString = (event, name, time, date) => {
+  const start = `Evento ${name} agendado para as ${time} horas`
+  const end = date
+    ? ` do dia ${date}`
+    : `, sem data.`
+
+  return `${start}${end}`
+}
+
+const fn = async ({ repositories, match }) => {
+  const [name, time, date] = match[1].split('::')
+
+  if (!name || !time) {
+    return `Informe o evento no padrão nome::hora::data! (A data é opcional)`
+  }
+
+  if (!date) {
+    const event = await repositories.events.create({ name, time })
+    return makeCreatedString(event, name, time)
+  }
+
+  const parsedDate = moment(date, 'DD/MM/YYYY').toDate()
+
+  const event = await repositories.events.create({ name, time, date: parsedDate })
+
+  return makeCreatedString(event, name, time, date)
+}
+
+module.exports = fn
+module.exports.regex = /\/newevent (.*)/
