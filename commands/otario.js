c1c3fa608d240493a4add16181fb32901ddd2af7
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2018-03-29 10:42:26 -0300

    Add new /otario command

---
+++ commands/otario.js
@@ -0,0 +1,16 @@
+const FILE_ID = 'CAADAQADXAIAAggBeQdw-0KxiDBssgI'
+
+const fun = ({ responseTypes }) => {
+  return [ {
+    type: responseTypes.TEXT,
+    content: 'Achou que não ia ter referência de choque de cultura nesse bot?!'
+  }, {
+    type: responseTypes.STICKER,
+    content: FILE_ID
+  } ]
+}
+
+fun.regex = /^\/(otario|choque)/ig
+fun.reply = true
+
+module.exports = fun
