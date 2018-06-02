9965a2edd142e510058897ac8343cbd2f368e5d2
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2018-06-02 17:07:12 -0300

    Start new architechture

---
+++ app/commands/echo.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = (ctx, next) => {
+  ctx.reply(ctx.message.text)
+}
---
+++ app/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const botify = require('@rjmunhoz/botify')
+
+module.exports = botify((app, config) => {
+
+})
