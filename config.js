9d62c8cbcf1b57993d1f1caa628ea81bae358343
Rogério Muhoz <rogerio.j.munhoz@gmail.com>
2019-05-20 10:36:27 -0300

    Update envs

--- config.js
+++ config.js
@@ -2,7 +2,12 @@
 
 module.exports = {
   telegram: {
-    API_TOKEN: process.env.API_TOKEN
+    API_TOKEN: process.env.API_TOKEN,
+    webhook: {
+      hostname: process.env.TELEGRAM_WEBHOOK_HOSTNAME,
+      bindingHost: process.env.TELEGRAM_WEBHOOK_BINDINGHOST,
+      port: parseInt(process.env.TELEGRAM_WEBHOOK_PORT || process.env.PORT || 3000, 10)
+    }
   },
   endpoints: {
     garoa: process.env.GAROA_API || 'http://garoahc.appspot.com/status'
